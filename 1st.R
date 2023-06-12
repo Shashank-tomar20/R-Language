0831306b0263dc84665ddf7e7b28c90d61163b86
shashank <98258341+Shashank-tomar20@users.noreply.github.com>
2023-06-12 15:11:35 +0530

    R language repository

---
+++ 1st.R
@@ -0,0 +1,8 @@
+data(mtcars)
+split<- sample.split(mtcars,SplitRatio = 0.7)
+split
+
+training <- subset(mtcars,split=="TRUE")
+testing <- subset(mtcars, split=="FALSE")
+
+model <- glm(vs ~ wt + disp,training,family="binomial")
